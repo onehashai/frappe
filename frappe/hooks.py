7794cde35e222df79ccc0d54f40b16e3283f301c
MaheKumarSharma <frappe@ip-172-31-24-100.ap-south-1.compute.internal>
2021-06-30 12:31:43 +0530

    Remove extra schduler events

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -189,12 +189,7 @@ scheduler_events = {
 			"frappe.oauth.delete_oauth2_data",
 			"frappe.website.doctype.web_page.web_page.check_publish_status",
 			"frappe.twofactor.delete_all_barcodes_for_users"
-		],
-		"0/1 * * * *": [
-			"frappe.email.queue.flush",
-			"frappe.email.doctype.email_account.email_account.pull"
 		]
-
 	},
 	"all": [
 		"frappe.email.queue.flush",
