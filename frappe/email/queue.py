23d19d7539d8ee416131bab7de5e7ad52d05e540
srijan <srijan@onehash.ai>
2021-07-02 16:58:08 +0530

    .

--- frappe/email/queue.py
+++ frappe/email/queue.py
@@ -519,6 +519,7 @@ def prepare_message(email, recipient, recipients_list):
 	email_account = get_outgoing_email_account(raise_exception_not_set=False, sender=email.sender)
 	if frappe.conf.use_ssl and email_account.track_email_status:
 		# Using SSL => Publically available domain => Email Read Reciept Possible
+		cur_site = frappe.local.site
 		if frappe.local.site == "admin_onehash":
 			cur_site = "admin.onehash.ai"
 		message = message.replace("<!--email open check-->", quopri.encodestring('<img src="https://{}/api/method/journeys.journeys.email_read.mark_email_as_seen?name={}&queue={}&recipient={}"/>'.format(cur_site, email.communication, email.name, recipient).encode()).decode())
