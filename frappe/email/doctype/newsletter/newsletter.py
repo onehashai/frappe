3691e748f22f4d785378ac6fcb8dc3bc9709b4c1
Rishabh Pandram <rishabh@onehash.ai>
2025-01-23 12:39:28 +0530

    newsletter.py reverted

--- frappe/email/doctype/newsletter/newsletter.py
+++ frappe/email/doctype/newsletter/newsletter.py
@@ -439,13 +439,13 @@ def newsletter_email_read(recipient_email=None, reference_doctype=None, referenc
 	verify_request()
 	try:
 		doc = frappe.get_cached_doc("Newsletter", reference_name)
-		# if doc.add_viewed(recipient_email, force=True, unique_views=True):
-		newsletter = frappe.qb.DocType("Newsletter")
-		(
-			frappe.qb.update(newsletter)
-			.set(newsletter.total_views, newsletter.total_views + 1)
-			.where(newsletter.name == doc.name)
-		).run()
+		if doc.add_viewed(recipient_email, force=True, unique_views=True):
+			newsletter = frappe.qb.DocType("Newsletter")
+			(
+				frappe.qb.update(newsletter)
+				.set(newsletter.total_views, newsletter.total_views + 1)
+				.where(newsletter.name == doc.name)
+			).run()
 
 	except Exception:
 		frappe.log_error(
